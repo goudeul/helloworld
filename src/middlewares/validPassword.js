232e2e7a27a7cdca5ff162e5aa8674f7f4588739
DESKTOP-BC63AV4\USER <goudeul@daum.net>
2021-07-15 20:15:32 +0900

    20210715 아이디,암호 체크

--- src/middlewares/validPassword.js
+++ src/middlewares/validPassword.js
@@ -4,7 +4,10 @@ export async function validPassword (ctx, next) {
   const body = ctx.request.body
   const { password, new_password } = body.user
 
-  if (!validUser.validPassword(new_password)) ctx.throw(404, '패스워드를 확인해주세요.', { code: 'S9999', ctx })
+  // if (!validUser.validPassword(new_password)) ctx.throw(404, '패스워드를 확인해주세요.', { code: 'S9999', ctx })
+
+  let resultPw = validUser.validPassword(new_password)
+  if (resultPw !=='') ctx.throw(404, resultPw, { code: 'S9999', ctx })
 
   await next()
 }
--- src/middlewares/validRegister.js
+++ src/middlewares/validRegister.js
@@ -4,8 +4,14 @@ export async function validRegister (ctx, next) {
   const body = ctx.request.body
   const { id, password, role, phone, identityNumber } = body.user
 
-  if (!validUser.validPassword(password)) ctx.throw(404, '패스워드를 확인해주세요.', { code: 'S9999', ctx })
-  if (!validUser.validID(id)) ctx.throw(404, '아이디에 사용할 수 없는 문자가 포함되었습니다.', { code: 'S9999', ctx })
+  // if (!validUser.validID(id)) ctx.throw(404, '아이디에 사용할 수 없는 문자가 포함되었습니다.', { code: 'S9999', ctx })
+  // if (!validUser.validPassword(password, id)) ctx.throw(404, '패스워드를 확인해주세요.', { code: 'S9999', ctx })
+  
+  let resultId = validUser.validID(id)
+  if (resultId !=='') ctx.throw(404, resultId, { code: 'S9999', ctx })
+  
+  let resultPw = validUser.validPassword(password, id)
+  if (resultPw !=='') ctx.throw(404, resultPw, { code: 'S9999', ctx })
 
   if (role === '10') {          // 교수 전화번호 체크
     if (!phone) ctx.throw(404, '교수회원은 전화번호가 필수입니다.', { code: 'S9999', ctx })
