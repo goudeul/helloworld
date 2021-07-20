d83745d6c51f15ca1e4c91c213d5a21cfc47fc98
DESKTOP-BC63AV4\USER <goudeul@daum.net>
2021-07-20 10:34:15 +0900

    20210720 로그인에러코드 추가

--- src/app.js
+++ src/app.js
@@ -1,50 +1,53 @@
-import Koa from 'koa'
-import Router from '@koa/router'
-import KoaBody from 'koa-body'
-import KoaCors from '@koa/cors'
-import error from 'koa-json-error'
-import KoaLogger from 'koa-logger'
-import { logger, stream } from './middlewares/winston'
-import Morgan from 'koa-morgan'
-import passport from 'koa-passport'
-import { authenticateJwt } from './middlewares/passport'
-import { apiRequest } from './middlewares/apiRequest'
-import './config/env'
+import Koa from "koa";
+import Router from "@koa/router";
+import KoaBody from "koa-body";
+import KoaCors from "@koa/cors";
+import error from "koa-json-error";
+import KoaLogger from "koa-logger";
+import { logger, stream } from "./middlewares/winston";
+import Morgan from "koa-morgan";
+import passport from "koa-passport";
+import { authenticateJwt } from "./middlewares/passport";
+import { apiRequest } from "./middlewares/apiRequest";
+import "./config/env";
 
-const app = new Koa()
+const app = new Koa();
 
 // authentication
-app.use(passport.initialize())
-app.use(authenticateJwt)
-app.use(apiRequest)
+app.use(passport.initialize());
+app.use(authenticateJwt);
+app.use(apiRequest);
 
 // middleware
-app.use(KoaCors())
-app.use(KoaBody())
+app.use(KoaCors());
+app.use(KoaBody());
 
 // logging console
 app.use(
   KoaLogger((str, args) => {
-    console.log(str)
-  }),
-)
+    console.log(str);
+  })
+);
 
 // Morgan으로 기록할 데이터 정의
-let context = {}
+let context = {};
 app.use((ctx, next) => {
-  context = ctx
-  return next()
-})
+  context = ctx;
+  return next();
+});
 
-Morgan.token('id', function getId () {
-  return (context.user) ? context.user.id : null
-})
+Morgan.token("id", function getId() {
+  return context.user ? context.user.id : null;
+});
 
 app.use(
-  Morgan('::remote-addr - :remote-user [:date[clf]] ' +
-    '":method :url HTTP/:http-version" :status :res[content-length] ' +
-    '":referrer" ":user-agent" ":id"', { stream: stream }),
-)
+  Morgan(
+    "::remote-addr - :remote-user [:date[clf]] " +
+      '":method :url HTTP/:http-version" :status :res[content-length] ' +
+      '":referrer" ":user-agent" ":id"',
+    { stream: stream }
+  )
+);
 
 // 에러처리
 app.use(
@@ -53,25 +56,25 @@ app.use(
       message: err.message,
       ctx: err.ctx,
       user: context.user,
-    }
+    };
     logger.log({
-      level: 'error',
+      level: "error",
       message: JSON.stringify(message),
-    })
+    });
 
     return {
-      code: err.code || 'S9999',
+      code: err.code || "S9999",
       message: err.message,
-    }
-  }),
-)
+    };
+  })
+);
 
 // router
-const router = new Router()
-const api = require('./routes/v1')
+const router = new Router();
+const api = require("./routes/v1");
 
-router.use('/v1', api.routes())
-app.use(router.routes()).use(router.allowedMethods())
+router.use("/v1", api.routes());
+app.use(router.routes()).use(router.allowedMethods());
 
 // central error handler, 동작하지 않음, app.use(err(...)) 에서 처되됨
 // app.on('error', (err, ctx) => {
@@ -81,12 +84,12 @@ app.use(router.routes()).use(router.allowedMethods())
 // })
 
 app.use(function (next) {
-  next.throw(404, { message: '허용하지 않은 접근' })
-})
+  next.throw(404, { message: "허용하지 않은 접근" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
   logger.log({
-    level: 'info',
+    level: "info",
     message: `Listening on port ${process.env.PORT}...`,
-  })
-})
+  });
+});
