d6300d5583fd5bac0172b3daf94da47272930779
DESKTOP-BC63AV4\USER <goudeul@daum.net>
2021-07-26 16:23:45 +0900

    20210726 회원가입 권한체크 관련점검

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -6,10 +6,11 @@ import auth from '../../middlewares/auth'
 
 const router = new Router()
 
-router.post('/register', validRegister, UserController.create)      // 회원가입
+// router.post('/register', validRegister, UserController.create)      // 회원가입 (권한체크없음)
+router.post('/register', auth['admin', 'professor'], validRegister, UserController.create)      // 회원가입
 router.post('/login', UserController.login)                         // 로그인
 router.get('/me', UserController.me)                                // 본인정보 확인
-router.put('/password', auth["all"], validPassword, UserController.changePassword)              // 비밀번호 변경
+router.put('/password', auth['all'], validPassword, UserController.changePassword)              // 비밀번호 변경
 router.put('/:id', auth['admin'], UserController.update)                           // 회원 정보 변경
 router.delete('/:id', auth['admin'], UserController.delete)                        // 회원 삭제
 
