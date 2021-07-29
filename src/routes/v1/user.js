9a4336d7a863cf89e270f8def99be7734bfe4899
Mac,Seo <ohhapday@gmail.com>
2021-07-29 12:56:25 +0900

    admin 계정 추가 및 gitignore 수정

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -6,8 +6,8 @@ import auth from '../../middlewares/auth'
 
 const router = new Router()
 
-// router.post('/register', validRegister, UserController.create)      // 회원가입 (권한체크없음)
-router.post('/register', auth['admin', 'professor'], validRegister, UserController.create)      // 회원가입
+router.post('/register', validRegister, UserController.create)      // 회원가입 (권한체크없음)
+// router.post('/register', auth['admin'], validRegister, UserController.create)      // 회원가입
 router.post('/login', UserController.login)                         // 로그인
 router.get('/me', UserController.me)                                // 본인정보 확인
 router.put('/password', auth['all'], validPassword, UserController.changePassword)              // 비밀번호 변경
