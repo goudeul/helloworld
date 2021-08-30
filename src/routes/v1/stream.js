d67ca57db61e3b9619b1c325bbdf041c45d6e405
Mac,Seo <ohhapday@gmail.com>
2021-08-30 09:55:33 +0900

    stream 파일 변경

--- src/routes/v1/stream.js
+++ src/routes/v1/stream.js
@@ -5,13 +5,13 @@ import path from 'path'
 const router = new Router()
 
 const appDir = path.dirname(require.main.filename)
-const file = appDir + '/../files/sample.zip'
+const file = appDir + '/../files/engine_main.zip'
 
 console.log(file)
 
 router.get('/', (ctx) => {
   const src = fs.createReadStream(file)
-  ctx.response.set('Content-disposition', 'attachment; filename=sample.zip')
+  ctx.response.set('Content-disposition', 'attachment; filename=engine_main.zip')
   ctx.body = src
 })
 
